93cc25b96980e09fb9c5f88e6e6d32c11ac5979f
joanis96 <57779348+joanis96@users.noreply.github.com>
2022-02-07 18:31:36 -0500

    Completed
    Finally

--- Apollo-projectJeanPolice/assets/js/functions.js
+++ Apollo-projectJeanPolice/assets/js/functions.js
@@ -29,8 +29,43 @@ function smoothScroll(){
 
 function fullSlider(){
  // A FAIRE
-	
-
+ const slides = document.getElementsByClassName("carousel-item");
+ const nextBtn = document.getElementById("carousel-button-next");
+ const prevBtn = document.getElementById("carousel-button-prev");
+ let position = 0;
+ const slidesLength = slides.length;
+ 
+ function hideAllSlides() {
+	 for(const slide of slides) {
+		 slide.classList.remove("carousel-item-visible");
+		 slide.classList.add("carousel-item-hidden");
+	 }
+ }
+ 
+ const nextSlide = function(e) {
+	 hideAllSlides();
+	 if(position === slidesLength - 1) {
+		 position = 0;
+	 } else {
+		 position++;
+	 }
+ 
+	 slides[position].classList.add("carousel-item-visible");
+ }
+ 
+ const prevSlide = function(e) {
+	 hideAllSlides();
+	 if(position === 0) {
+		 position = slidesLength - 1;
+	 } else {
+		 position--;
+	 }
+ 
+	 slides[position].classList.add("carousel-item-visible");
+ }
+ 
+ nextBtn.addEventListener("click", nextSlide);
+ prevBtn.addEventListener("click", prevSlide);
 }
 
 
